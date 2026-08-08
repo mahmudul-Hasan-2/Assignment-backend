5204feb46b0232b199a79ec2abf0f6d7c561c30f
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-08-09 01:48:49 +0600

    something for deploy 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,29 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+const connectionString = process.env.DATABASE_URL!;
 
-const prisma = new PrismaClient({
-  adapter,
-});
+// 1. pg package থেকে Pool তৈরি করতে হবে
+const pool = new Pool({ connectionString });
+
+// 2. তারপর সেই pool টা adapter-এ পাস করতে হবে
+const adapter = new PrismaPg(pool);
+
+// 3. Vercel/Serverless এর জন্য Singleton Pattern (যাতে বারবার কানেকশন ওপেন না হয়)
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
